9ea110b3136c71a43a65e26773203eabacbba7e4
Zhiyao Tang <32091356+zhiyaot@users.noreply.github.com>
2022-01-22 00:07:55 -0500

    Update cis571 ssh.cmd

--- cis571 ssh.cmd
+++ cis571 ssh.cmd
@@ -3,7 +3,10 @@ set pennkey=zhiyaot
 
 if not exist %USERPROFILE%\.ssh\id_rsa (
     ssh-keygen -t rsa -q -f %USERPROFILE%\.ssh\id_rsa -N ""
+    echo enter your pennkey password here
+    type %USERPROFILE%\.ssh\id_rsa.pub | ssh %pennkey%@eniac.seas.upenn.edu "cat >> ~/.ssh/authorized_keys"
 ) else (
+    echo enter your pennkey password here
     type %USERPROFILE%\.ssh\id_rsa.pub | ssh %pennkey%@eniac.seas.upenn.edu "cat >> ~/.ssh/authorized_keys"
     echo you already got setup, dont run this again
 )
